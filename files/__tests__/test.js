0666fc283ed0e8226462eb220730ae76361baf78
Helder Burato Berto <helder.burato@gmail.com>
2019-03-20 11:36:30 -0300

    refactor: tests moved to __tests__

--- files/__tests__/test.js
+++ files/__tests__/test.js
@@ -2,13 +2,13 @@
 let test = require('ava');
 let { createElement: h } = require('react');
 let ReactTestRenderer = require('react-test-renderer');
-let use... = require('./');
+let use... = require('../src');
 
 function render(val) {
   return ReactTestRenderer.create(val);
 }
 
-test(t => {
+test('it should render React component using hook', t => {
   function Component() {
     let value = use...();
     return h('div');
